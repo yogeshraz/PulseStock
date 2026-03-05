21454c2b23df6a49e2d29339004bd45b688059d1
Yogesh Raj <yogesh@wellnessatwork.ai>
2026-03-05 10:14:34 +0530

    Initial Commit

---
+++ PulseStock/PulseStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseStockApp.swift
+//  PulseStock
+//
+//  Created by Yogesh Raj on 05/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PulseStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
